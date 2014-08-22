f4110bd0524a9fcbc7fdad50d4f6aa46ae2c47e5
Satyajit Sahoo <satyajit.happy@gmail.com>
2014-08-22 14:37:07 +0530

    Start working on customization app

---
+++ customization/customization.js
@@ -0,0 +1,50 @@
+var less = require("less"),
+	fs = require("fs");
+
+module.exports = function(core) {
+	var parser = new less.Parser({
+			compress: true,
+			yuicompress: true
+		}),
+		lessBase = fs.readFileSync(__dirname + "/customization.template", "utf8"),
+		genCss = function(action, next) {
+			var room = action.room,
+				lessVars = "",
+				lessSrc,
+				options;
+
+			if (!(room && room.guides && room.guides.customization && room.guides.customization.options)) {
+				return next();
+			}
+
+			options = room.guides.customization.options;
+
+			for (var opt in options) {
+				lessVars += "@" + opt + ":" + options[opt] + ";";
+			}
+
+			if (!lessVars) {
+				return next();
+			}
+
+			lessSrc = lessVars + lessBase;
+
+			parser.parse(lessSrc, function(error, cssTree) {
+				var css;
+
+				if (error) {
+					less.writeError(error);
+					return;
+				}
+
+				css = cssTree.toCSS();
+
+				room.guides.customization.css = (css) ? css.replace("<", "\\3c").replace(">", "\\3e") : "";
+			});
+
+			next();
+		};
+
+	core.on("room", genCss, "modifier");
+	core.on("back", genCss, "modifier");
+};
